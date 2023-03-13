855b6c91efaf5e2ffc5e33280430507c008272a6
Plato Kiorpelidis <kioplato@gmail.com>
2023-03-13 16:02:35 +0200

    add option to specify # of clusters through run.sh
    Also improve the script as a whole.
    
    Signed-off-by: Plato Kiorpelidis <kioplato@gmail.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-cd Build
-make clean
-make -j8
-cd ..
-./Build/knng datasets/dummy-data.bin 8
+N_CLUSTERS=$1
+
+# The absolute path to this script.
+ABSOLUTE_PATH=$(readline -f "$0")
+# Project's root directory (this script lives in the project's root).
+DIRECTORY=$(dirname "$ABSOLUTE_PATH")
+
+# Compile the project.
+(cd "$DIRECTORY" || exit 1; make clean; make -j8)
+
+# Run the solution.
+./Build/knng datasets/dummy-data.bin "$N_CLUSTERS"
